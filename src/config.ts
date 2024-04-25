275782bb5a60b3641cc759205d5274d5c0667660
xcrystal627 <167706380+xcrystal627@users.noreply.github.com>
2024-04-25 15:18:24 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,23 +9,23 @@ export const JWT_API = {
 };
 
 export const FIREBASE_API = {
-  apiKey: 'AIzaSyBernKzdSojh_vWXBHt0aRhf5SC9VLChbM',
+  apiKey: '',
   authDomain: 'berry-material-react.firebaseapp.com',
   projectId: 'berry-material-react',
   storageBucket: 'berry-material-react.appspot.com',
-  messagingSenderId: '901111229354',
-  appId: '1:901111229354:web:a5ae5aa95486297d69d9d3',
-  measurementId: 'G-MGJHSL8XW3'
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 export const AUTH0_API = {
-  client_id: '7T4IlWis4DKHSbG8JAye4Ipk0rvXkH9V',
-  domain: 'dev-w0-vxep3.us.auth0.com'
+  client_id: '',
+  domain: ''
 };
 
 export const AWS_API = {
-  poolId: 'us-east-1_AOfOTXLvD',
-  appClientId: '3eau2osduslvb7vks3vsh9t7b0'
+  poolId: '',
+  appClientId: ''
 };
 
 // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
